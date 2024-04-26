9e81baba9b2dc497d219dbc55eadf4370c10f0c7
nitin2407negi <nitinnegi241@gmail.com>
2024-04-26 17:29:46 +0530

    loops

--- day34_advanceLoops/loops.js
+++ day34_advanceLoops/loops.js
@@ -1,7 +1,10 @@
 // for=>jb number of iteration known history
 // while=>number of iteration unknown
 // do while=>atleast run once
+
 //foreach==>index,element,array
+let arrr=[10,20,"superman","spiderman"]
+arrr.forEach((value,index) =>  console.log(index,value));
 
 //for-in ==object ke liye
 
@@ -16,6 +19,10 @@ for(let key in Marvel){
 //    console.log(`${key}: ${Marvel[key]}`);
 //    console.log(`${key}`);
 }
+
 //for-of==array ko iterate krne ke liye
 
 let arr=[10,20,"superman","spiderman"]
+for(let element of arr){
+    // console.log(element);
+}
