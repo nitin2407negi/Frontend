7dacdd8127eb090479fb84258923f1191b69c2be
nitin2407negi <nitinnegi241@gmail.com>
2024-05-23 17:18:32 +0530

    callback

--- day44_JS_callback/calback.js
+++ day44_JS_callback/calback.js
@@ -1,3 +1,36 @@
 // callback=>ek function ke parameter me dusra function call krna use callback khte hai
 
-console.log("callback function")
+
+//function=1
+// const print=(content,callback)=>{
+//     console.log(content)
+//     callback();
+// }
+// const print2=()=>console.log("spiderman is belong from Marvel");
+// print("super is belong from DC",print2)
+
+//function=2
+
+const marksClc=(callback)=>{
+   const result=callback();
+   console.log("this is result array",result);
+   let totalMarks=0;
+   let count=0;
+   for(let key in result){
+    totalMarks+=result[key]
+    count++;
+   }
+   return totalMarks/count;
+};
+
+const subjects=()=>{
+    const arr={
+        cpp:59,
+        java:83,
+        python:79,
+        js:97,
+        node:69
+    }
+    return arr;
+}
+console.log(marksClc(subjects))
